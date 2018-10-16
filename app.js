3aee10256c674d5d5bdffbc131cf261f7ceb293c
Tom Bialick <tombialick@Toms-iMac.local>
2018-10-16 08:06:19 -0400

    fixing / route

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var phoneRouter = require('./routes/phone');
 
 var app = express();
 
-app.use('/', express.static(path.join(__dirname + '/../client/', 'build')));
+//app.use('/', express.static(path.join(__dirname + '/../client/', 'build')));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,7 +20,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, 'client')));
 
 //app.use('/', indexRouter);
 app.use('/users', usersRouter);
