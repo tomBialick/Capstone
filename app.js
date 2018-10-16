2c3a2363346fac08450da0eeb10217caba5a34bb
Tom Bialick <tombialick@Toms-iMac.local>
2018-10-16 08:07:47 -0400

    slight revert for / route

--- app.js
+++ app.js
@@ -20,9 +20,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use('/', express.static(path.join(__dirname, 'client')));
+app.use(express.static(path.join(__dirname, 'client')));
 
-//app.use('/', indexRouter);
+app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/phone', phoneRouter);
 
