b00fb3c858ebb683746c4c5f070aa1e51277f8a1
Tom Bialick <tombialick@Toms-iMac.local>
2018-10-16 07:45:07 -0400

    changed / get to send index.html

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
-/* GET home page.
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});*/
+  //res.render('index', { title: 'Express' });
+  res.sendFile(path.join(__dirname + '../client/public/index.html'));
+});
 
 router.post('/', function(req, res, next) {
   console.log(req.body.text)
