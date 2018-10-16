a2fd7978d1e71972f8a4378126a955df3e776146
Tom Bialick <tombialick@Toms-iMac.local>
2018-10-16 07:39:44 -0400

    removed / get

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page.
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
-});
+});*/
 
 router.post('/', function(req, res, next) {
   console.log(req.body.text)
