398814b7db61c4430005752e12f0d2901a269530
Tom Bialick <tombialick@Toms-iMac.local>
2018-10-16 06:33:24 -0400

    added componentDidUpdate

--- client/src/App.js
+++ client/src/App.js
@@ -3,28 +3,33 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  constructor(props){
-        super(props);
-        this.state = {
-          x:0,
-          y:0,
-          z:0
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      x: 0,
+      y: 0,
+      z: 0
+    };
+  }
 
   componentDidMount() {
-    fetch('http://5halfcap.ngrok.io/phone', {method: 'GET'}).then((response) => response.json())
-    .then((responseJson) => {
+    fetch('http://5halfcap.ngrok.io/phone', {method: 'GET'}).then((response) => response.json()).then((responseJson) => {
       this.setState({x: responseJson.body.x, y: responseJson.body.y, z: responseJson.body.z});
       //console.log(responseJson);
     });
   }
+
+  componentDidUpdate(prevProps, prevState) {
+    fetch('http://5halfcap.ngrok.io/phone', {method: 'GET'}).then((response) => response.json()).then((responseJson) => {
+      this.setState({x: responseJson.body.x, y: responseJson.body.y, z: responseJson.body.z});
+      //console.log(responseJson);
+    });
+  }
+
   render() {
-    return (
-      <h1>X: {this.state.x}</h1>
-      <h1>Y: {this.state.y}</h1>
-      <h1>Z: {this.state.z}</h1>
-    );
+    return (<h1>X: {this.state.x}</h1>
+    <h1>Y: {this.state.y}</h1>
+    <h1>Z: {this.state.z}</h1>);
     /*
       <div className="App">
         <header className="App-header">
