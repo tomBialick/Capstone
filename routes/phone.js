6b395850fc106218f44b7367d046da25834e31d2
Tom Bialick <tombialick@Toms-iMac.local>
2018-11-11 15:01:37 -0500

    changed sensors on backend

--- routes/phone.js
+++ routes/phone.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-var X = 0;
-var Y = 0;
-var Z = 0;
+var gX = 0;
+var gY = 0;
+var gZ = 0;
+var tX = 0;
+var tY = 0;
+var tZ = 0;
 
 router.get('/', (req, res, next) => {
-  res.send({body: {x: X, y: Y, z: Z}})
+  res.send({body: {gx: gX, gy: gY, gz: gZ, tx: tX, ty: tY, tz: tZ}})
 })
 
 /*
@@ -22,9 +25,12 @@ router.get('/', (req, res, next) => {
 
 router.post('/', (req, res, next) => {
 
-  X = req.body.x
-  Y = req.body.y
-  Z = req.body.z
+  gX = req.body.gx
+  gY = req.body.gy
+  gZ = req.body.gz
+  tX = req.body.tx
+  tY = req.body.ty
+  tZ = req.body.tz
 
   res.send("ok")
 
