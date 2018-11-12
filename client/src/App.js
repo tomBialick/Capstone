2335d23752a4f9a8dda6ba4303a28f67cb1bf8bf
Tom Bialick <tombialick@Toms-iMac.local>
2018-11-11 22:59:24 -0500

    fixed css for jsx

--- client/src/App.js
+++ client/src/App.js
@@ -109,8 +109,8 @@ class App extends Component {
 */
   render() {
     return (
-      <div style="width:100%;height:100%">
-        <canvas ref="analyzerCanvas" id="analyzer" style="width:100%;height:100%">
+      <div style={{width:'100%',height:'100%'}}>
+        <canvas ref="analyzerCanvas" id="analyzer" style={{width:'100%',height:'100%'}}>
         </canvas>
       </div>
     );
